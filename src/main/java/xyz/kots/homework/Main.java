c41f4fde58c54115cee1e6b8e01343ba31a01244
kots <kots___@mail.ru>
2016-12-11 14:36:24 +0200

    conf

--- src/main/java/xyz/kots/homework/Main.java
+++ src/main/java/xyz/kots/homework/Main.java
@@ -1,5 +1,7 @@
 package xyz.kots.homework;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import xyz.kots.homework.entity.MyDirectory;
 import xyz.kots.homework.entity.SimpleDirectory;
 
@@ -10,7 +12,12 @@ import java.util.Arrays;
 
 public class Main {
 
+    private final static Logger LL = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException {
+
+        LL.debug("hello");
+
         InputFile inputFile = new InputFile("./resources/input.txt");
         inputFile.read();
         inputFile.print();
